b07c59408636f232dc061cb04d3e2ad65c9c81ea
cathayrisk <cathay00c@outlook.com>
2025-04-21 00:44:03 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,8 +11,6 @@ import re
 import sys
 import io
 
-st.sidebar.success("Select a demo above.")
-
 #############################################################################
 # 1. Define the GraphState (minimal fields: question, generation, websearch_content)
 #############################################################################
