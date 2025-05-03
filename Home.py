f4a1692a0b4bd31ce2c1bb041111b574bbab8c3a
cathayrisk <cathay00c@outlook.com>
2025-05-03 11:15:35 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,6 +7,7 @@ from duckduckgo_search import DDGS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.callbacks.base import BaseCallbackHandler
 import re
 from datetime import datetime
 import inspect
