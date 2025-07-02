5496bbaf027b0ecc97ad22f01c9c6d1ada8f7697
cathayrisk <cathay00c@outlook.com>
2025-07-02 13:45:56 +0800

    Update 4_Matlab_VaR.py

--- pages/4_Matlab_VaR.py
+++ pages/4_Matlab_VaR.py
@@ -12,7 +12,6 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 st.title("Supabase Storage 檔案上傳工具 🥜")
 
 uploaded_file = st.file_uploader("請選擇要上傳的檔案", type=None)
-uploaded_file = st.file_uploader("請選擇要上傳的檔案", type=None)
 
 if uploaded_file is not None:
     file_bytes = uploaded_file.getvalue()
