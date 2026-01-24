82c512ea613f0cec3dc78782d6b00e8fa1a1642c
cathayrisk <cathay00c@outlook.com>
2026-01-24 22:08:30 +0800

    Update __init__.py

--- docstore/__init__.py
+++ docstore/__init__.py
@@ -1,1 +1,41 @@
+# docstore/__init__.py
+# -*- coding: utf-8 -*-
+"""
+docstore package exports.
 
+讓你可以在 pages/Anya_Test.py 用：
+from docstore import FileRow, build_indices_incremental, ... 以及 badges_markdown
+"""
+
+from .docstore import (
+    FileRow,
+    DocStore,
+    build_file_row_from_bytes,
+    build_indices_incremental,
+    doc_list_payload,
+    doc_search_payload,
+    doc_get_fulltext_payload,
+    HAS_UNSTRUCTURED_LOADERS,
+    HAS_PYMUPDF,
+    HAS_FLASHRANK,
+    HAS_BM25,
+    estimate_tokens_from_chars,
+)
+
+from .badges import badges_markdown
+
+__all__ = [
+    "FileRow",
+    "DocStore",
+    "build_file_row_from_bytes",
+    "build_indices_incremental",
+    "doc_list_payload",
+    "doc_search_payload",
+    "doc_get_fulltext_payload",
+    "HAS_UNSTRUCTURED_LOADERS",
+    "HAS_PYMUPDF",
+    "HAS_FLASHRANK",
+    "HAS_BM25",
+    "estimate_tokens_from_chars",
+    "badges_markdown",
+]
